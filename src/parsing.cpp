8c49d2d6d8fb97591c5c2b928407c016c908024a
Johnny Varila <jvarila@student.hive.fi>
2025-09-08 11:02:52 +0300

    Added new parser

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -1,4 +1,4 @@
-#include "minisynth.hpp"
+#include "../inc/minisynth.hpp"
 #include <string>
 #include <fstream>
 #include <map>
@@ -62,13 +62,12 @@ std::string	fill_note(std::string notestr, std::vector<Notes> note, size_t idx)
 	return notestr;
 }
 
-Data	parseFile(std::string content)
+size_t	find_tempo(std::string content, Data& data)
 {
-	Data	test;
+	size_t	i = content.find('t');
 	size_t	j = 0;
 	size_t	k = 0;
 
-	size_t	i = content.find('t');
 	while (content[i])
 	{
 		if (content.substr(i, 6) == "tempo " && (i == 0 || content[i - 1] == '\n'))
@@ -78,92 +77,163 @@ Data	parseFile(std::string content)
 			while (std::isdigit(content[j]))
 				j++;
 			std::string	tmp = content.substr(k, (j - k));
-			test.tempo = std::stoi(tmp);
+			data.tempo = std::stoi(tmp);
 		}
 		i++;
 	}
 	i = j + 1;
+	return i;
+}
+
+size_t	find_track_amt(std::string content, Data& data, size_t i)
+{
+	size_t	j = 0;
+	size_t	k = 0;
+
 	while (content[i])
 	{
 		if (content.substr(i, 7) == "tracks " && content[i - 1] == '\n')
 		{
 			j = i + 7;
 			k = j;
-			test.track_amt = 1;
+			data.track_amt = 1;
 			while (content[j] != '\n')
 			{
 				if (content[j] == ',')
-					test.track_amt++;
+					data.track_amt++;
 				j++;
 			}
 			break;
 		}
 		i++;
 	}
-	test.tracks = new Track[test.track_amt];
-	j = i + 7;
-	for (size_t l = 0; l < test.track_amt && content[j]; l++)
-	{
-		k = j;
-		while (content[j] != ',' && content[j] != '\n')
-			j++;
-		std::string	tmp = content.substr(k, (j - k));
-		test.tracks[l].instrument = find_type(tmp);
-		test.tracks[l].track_dur = 0;
-		j++;
-	}
-	while (content[j])
-	{
-		while (content[j] && !(std::isdigit(content[j]) && content[j - 1] == '\n'))
-			j++;
-		if (!content[j])
-			break;
-		size_t	o = j;
-		while (content[j] != ':')
-			j++;
-		size_t	track_idx = std::stoi(content.substr(o, (j - o))) - 1;
-		j += 2;
-		k = j;
-		while (content[j] != '\n')
-			j++;
-		std::string	string = content.substr(k, (j - k));
-		std::istringstream	iss(string);
-		std::string	token;
-		std::map<std::string, double>	freq = make_freq();
+	i += 7;
+	return i;
+}
 
-		int	idx = 0;
-		while (iss >> token)
-		{
-			if (token[0] == '|')
-				continue;
-			size_t	pos = token.find('/');
-			double	frequency;
-			double	duration;
-			if (pos != std::string::npos)
-			{
-				std::string	note = fill_note(token.substr(0, pos), test.tracks[track_idx].notes, idx);
-				frequency = find_freq(note, freq);
-				duration = std::stod(token.substr(pos + 1));
-				test.tracks[track_idx].notes.push_back({note, duration, frequency});
-			}
-			else
-			{
-				std::string	note = fill_note(token, test.tracks[track_idx].notes, idx);
-				frequency = find_freq(note, freq);
-				if (idx > 0)
-					duration = test.tracks[track_idx].notes[idx - 1].duration;
-				else
-					duration = 1.0;
-				test.tracks[track_idx].notes.push_back({note, duration, frequency});
-			}
-			test.tracks[track_idx].track_dur += duration;
-			idx++;
-		}
-		while (content[j] != '\n')
-			j++;
-		j++;
+size_t	find_instrument(std::string content, Data &data, size_t i)
+{
+	size_t	k = 0;
+
+	for (size_t l = 0; l < data.track_amt && content[i]; l++)
+	{
+		k = i;
+		while (content[i] != ',' && content[i] != '\n')
+			i++;
+		std::string	tmp = content.substr(k, (i - k));
+		data.tracks[l].instrument = find_type(tmp);
+		data.tracks[l].track_dur = 0;
+		data.tracks[l].volume = 1;
+		i++;
 	}
-	return test;
+	return i;
+}
+
+size_t	find_volume(std::string content, Data& data, size_t i)
+{
+	size_t	k = 0;
+
+	if (content.substr(i, 7) == "volume " && content[i - 1] == '\n')
+	{
+		i += 7;
+		for (size_t l = 0; l < data.track_amt && content[i]; l++)
+		{
+			k = i;
+			while (content[i] != ',' && content[i] != '\n')
+				i++;
+			std::string	tmp = content.substr(k, (i - k));
+			data.tracks[l].volume = std::stoi(tmp);
+			i++;
+		}
+	}
+	return i;
+}
+
+void	find_notes(std::string string, Data& data, size_t track_idx)
+{
+	std::map<std::string, double>	freq = make_freq();
+	std::istringstream				iss(string);
+	std::string						token;
+
+	int	idx = 0;
+	while (iss >> token)
+	{
+		if (token[0] == '|')
+			continue;
+		size_t	pos = token.find('/');
+		double	frequency;
+		double	duration;
+		if (pos != std::string::npos)
+		{
+			std::string	note = fill_note(token.substr(0, pos), data.tracks[track_idx].notes, idx);
+			frequency = find_freq(note, freq);
+			duration = std::stod(token.substr(pos + 1));
+			data.tracks[track_idx].notes.push_back({note, duration, frequency});
+		}
+		else
+		{
+			std::string	note = fill_note(token, data.tracks[track_idx].notes, idx);
+			frequency = find_freq(note, freq);
+			if (idx > 0)
+				duration = data.tracks[track_idx].notes[idx - 1].duration;
+			else
+				duration = 1.0;
+			data.tracks[track_idx].notes.push_back({note, duration, frequency});
+		}
+		data.tracks[track_idx].track_dur += duration;
+		idx++;
+	}
+}
+
+Data	parseFile(std::string content)
+{
+	Data							data;
+	size_t							k = 0;
+
+	size_t	i = find_tempo(content, data);
+	i = find_track_amt(content, data, i);
+	data.tracks = new Track[data.track_amt];
+	i = find_instrument(content, data, i);
+	while (content[i] == '\n' && content[i] == '#')
+	{
+		if (content[i] == '#')
+		{
+			while (content[i] != '\n')
+				i++;
+		}
+		i++;
+	}
+	i = find_volume(content, data, i);
+	while (content[i])
+	{
+		while (content[i] && !(std::isdigit(content[i]) && content[i - 1] == '\n'))
+			i++;
+		if (!content[i])
+			break;
+		size_t	j = i;
+		while (content[i] != ':')
+			i++;
+		size_t	track_idx = std::stoi(content.substr(j, (i - j))) - 1;
+		i += 2;
+		k = i;
+		while (content[i] != '\n')
+			i++;
+		std::string	string = content.substr(k, (i - k));
+		find_notes(string, data, track_idx);
+		while (content[i] != '\n')
+			i++;
+		i++;
+	}
+	// for (size_t m = 0; m < data.track_amt; m++)
+	// {
+	// 	std::cout << "Track no " << m << ": instrument: " << data.tracks[m].instrument
+	// 		<< ", dur: " << data.tracks[m].track_dur<< ", volume: " << data.tracks[m].volume
+	// 		<< ", notes: \n";
+	// 	for (const auto& t : data.tracks[m].notes)
+	// 		std::cout << "duration: " << t.duration << ", freq: " << t.frequency
+	// 			<< ", pitch: " << t.pitch << std::endl;
+	// }
+	return data;
 }
 
 Data	parser(std::string input)
